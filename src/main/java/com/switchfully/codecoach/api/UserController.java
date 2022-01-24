7d96013427c13d64d11e1973d9e749ae1df3c7db
enoirhom <emil.noirhomme@student.unamur.be>
2022-01-24 11:48:44 +0100

    Story 5 - JD/EN - Fixing CORS problem

--- src/main/java/com/switchfully/codecoach/api/UserController.java
+++ src/main/java/com/switchfully/codecoach/api/UserController.java
@@ -1,15 +1,13 @@
 package com.switchfully.codecoach.api;
 
+import com.switchfully.codecoach.service.user.UserService;
+import com.switchfully.codecoach.service.user.dto.CreateUserDto;
+import com.switchfully.codecoach.service.user.dto.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.UUID;
-import com.switchfully.codecoach.service.user.UserService;
-import com.switchfully.codecoach.service.user.dto.CreateUserDto;
-import com.switchfully.codecoach.service.user.dto.UserDto;
 
 @RestController
 @RequestMapping(value = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
