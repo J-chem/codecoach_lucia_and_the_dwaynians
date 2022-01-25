eeddcf079eb3e4cd9fd31159e60de9a8213b5e1e
Lucia Madero <maderolucia@gmail.com>
2022-01-25 11:24:53 +0100

    Story13 - EN, LM - Created Status, Session and Location domain classes

---
+++ src/main/java/com/switchfully/codecoach/domain/session/Session.java
@@ -0,0 +1,95 @@
+package com.switchfully.codecoach.domain.session;
+
+import com.switchfully.codecoach.domain.user.User;
+
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "SESSION")
+public class Session {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "SESSION_ID")
+    private UUID id;
+
+    @Column(name = "TOPIC")
+    private String topic;
+
+    @Column(name = "DATE")
+    private LocalDate date;
+
+    @Column(name = "TIME")
+    private LocalTime time;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "LOCATION")
+    private Location location;
+
+    @Column(name = "REMARKS")
+    private String remarks;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "STATUS")
+    private Status status;
+
+    @ManyToOne
+    @JoinColumn(name = "FK_COACH_ID")
+    private User coach;
+
+    @ManyToOne
+    @JoinColumn(name = "FK_COACHEE_ID")
+    private User coachee;
+
+    public Session(String topic, LocalDate date, LocalTime time, Location location, String remarks) {
+        this.topic = topic;
+        this.date = date;
+        this.time = time;
+        this.location = location;
+        this.remarks = remarks;
+    }
+
+    // Hibernate required
+    public Session() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public User getCoach() {
+        return coach;
+    }
+
+    public User getCoachee() {
+        return coachee;
+    }
+}
