b45bf1d40a956336275aaa1174d5c7f06683c417
enoirhom <emil.noirhomme@student.unamur.be>
2022-01-26 09:46:52 +0100

    Story 13 - LM/EN - remove unused imports

--- src/main/java/com/switchfully/codecoach/service/session/dto/SessionDto.java
+++ src/main/java/com/switchfully/codecoach/service/session/dto/SessionDto.java
@@ -2,7 +2,6 @@ package com.switchfully.codecoach.service.session.dto;
 
 import com.switchfully.codecoach.domain.session.Location;
 import com.switchfully.codecoach.domain.session.Status;
-import com.switchfully.codecoach.domain.user.Topic;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
--- src/test/java/com/switchfully/codecoach/api/SessionControllerEndToEndTest.java
+++ src/test/java/com/switchfully/codecoach/api/SessionControllerEndToEndTest.java
@@ -1,6 +1,5 @@
 package com.switchfully.codecoach.api;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.switchfully.codecoach.domain.session.Location;
